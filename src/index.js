a303309fcd263d65fa76369e8b766b27b525519f
Joseph McCormick <esmevane@gmail.com>
2017-07-04 10:37:45 -0500

    Ex nihilo.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import "font-awesome/css/font-awesome.css";
+import './index.css';
+
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const root = document.getElementById('root');
+
+render(<App />, root);
+
+registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
+    render(<NextApp />, root);
+  });
+}
+
+registerServiceWorker();
