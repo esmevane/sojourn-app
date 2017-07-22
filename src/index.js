9eee88598684e82f1fb9b04f5347554d71d4045e
Joseph McCormick <esmevane@gmail.com>
2017-07-21 20:06:22 -0500

    Add Overpass Mono monospace font.
    Pretty small commit overall, just to add support for a nicer monospace
    font than the browser default.
    
    For future reference, if you want to do this, I moseyed on over to
    Google Fonts, grabbed the CSS for the font I liked, then downloaded all
    of the woff2 files, and renamed them to something more legible.
    
    For some reason Google makes this a mildly tedious thing, spending a lot
    of their UI trying to convince you that you need to link to some silly
    CSS generator which points to hashed nonsense routes on one of their
    CDNs.
    
    I'm not cranky; you're cranky.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 // @flow
 
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
-import "font-awesome/css/font-awesome.css";
-import 'index.css';
+import "font-awesome/css/font-awesome.css"
+import 'fonts/mono/overpass.css'
+import 'index.css'
 
-import App from 'components/App';
-import registerServiceWorker from 'registerServiceWorker';
+import App from 'components/App'
+import registerServiceWorker from 'registerServiceWorker'
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
 
-render(<App />, root);
+render(<App />, root)
 
-registerServiceWorker();
+registerServiceWorker()
 
 if (module.hot) {
   module.hot.accept('components/App', () => {
-    const NextApp = require('components/App').default;
-    render(<NextApp />, root);
-  });
+    const NextApp = require('components/App').default
+    render(<NextApp />, root)
+  })
 }
 
-registerServiceWorker();
+registerServiceWorker()
